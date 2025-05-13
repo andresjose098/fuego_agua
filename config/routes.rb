a41ce28e8634a617762dbc608fdd286c6b23b65e
andresjose098 <andres-jose1998@hotmail.com>
2025-05-12 23:28:55 -0500

    commit

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
 
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
-  delete 'logout', to: 'sessions#destroy'
+  get 'logout', to: 'sessions#destroy'
 
   get 'register', to: 'registrations#new'
   post 'register', to: 'registrations#create'
